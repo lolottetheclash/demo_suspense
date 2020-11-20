b5ac08fbd9f29f4e36785901af3c31325a6750cf
lauren <laurenzolivier@gmail.com>
2020-11-20 10:40:51 +0100

    improvement 1

--- src/App.js
+++ src/App.js
@@ -14,27 +14,22 @@ function App() {
     <div className="App">
       <h1>1st Method: Fetch on Render</h1>
       <ol>
-        <li>Le composant User lance son propre affichage</li>
+        <li>Le composant "User" lance son propre affichage</li>
 
         <li>
-          Dans cet affichage se trouve un composant enfant 'POSTS', il doit donc
-          lancer l'affichage de 'POSTS' avant de pouvoir terminer le sien
+          Une fois le composant monté, on lance la récupération des données du
+          user dans le useEffect.
         </li>
+        <li>Après 2 secondes, les données du user sont chargées.</li>
         <li>
-          Le composant 'POSTS' est affiché, on passe dans son useEffect pour
-          charger les données
+          Le composant "Posts" lance la récupération de ses propres données dans
+          le useEffect.
         </li>
-        <li>
-          Le composant 'POSTS' étant affiché, le composant User peut donc
-          terminer son propre affichage et passer dans son useEffect et charger
-          les données du profil utilisateur.
-        </li>
-        <li></li>
+        <li>Après 3 secondes, les données des Posts sont chargées.</li>
         <p style={{ color: 'red' }}>
           Inconvénient: on perd du temps car on commence par afficher le
-          composant et ses enfants AVANT de lancer le chargement des données.
-          Dans cet exemple, cela prend 5 secondes en tout pour charger les
-          données.
+          composant AVANT de lancer le chargement des données. Dans cet exemple,
+          cela prend 5 secondes en tout pour charger les données.
         </p>
       </ol>
       {timer >= 5 ? <h2>timer: 5</h2> : <h2>timer: {timer}</h2>}
